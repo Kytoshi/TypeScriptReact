192fc4b6ea045daa0f9c3fa0797ccd2dbd840f27
hachiko <62117951+Kytoshi@users.noreply.github.com>
2025-11-06 16:05:46 -0800

    Revert "Merge branch 'gh-pages' of https://github.com/Kytoshi/Valorant-Instalock-Game into gh-pages"
    This reverts commit c27a0f35f1b82cceb84c13c422a9a1e744e037bc, reversing
    changes made to 7207eace97b93dbccf201df3de9e04b50614c8e4.

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import type { JSX } from "react";
+
+export default function Header(): JSX.Element {
+  return (
+    <header>
+      <h1>Valorant: Agent Lobby</h1>
+      <p>
+        Guess the word within 8 attempts to get your instalock character pick.
+      </p>
+    </header>
+  );
+}
